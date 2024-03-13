2005e48831bdd99c528ecca3a20bbecc7d912456
ilamathi <ilamathi.ka@gmail.com>
2024-03-13 19:00:39 +0530

    commit change

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,17 @@
 #! /bin/bash
+
+docker login --username =ilamathivinoth --password =dckr_pat_G6kRN127hG362EA-fDWLyDZTrRo
+docker tag app:v1 ilamathivinoth/dev:v1
+docker push ilamathivinoth/dev:v1
+if [[ $GIT_BRANCH == "main"]]; then
+docker tag app:v1 ilamathivinoth/prod:v1
+docker push ilamathivinoth/prod:v1
+elif[[ $GIT_BRANCH == "dev"]]; then
+docker tag app:v1 ilamathivinoth/dev:v1
+docker push ilamathivinoth/dev:v1
+else
+echo "pipeline successful"
+fi
+
+
 docker compose up -d
