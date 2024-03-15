9d9a980451af020687293bb4cb83ce3616cd4d1a
ilamathi <ilamathi.ka@gmail.com>
2024-03-15 17:02:39 +0530

    commit msg

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker login --username =ilamathivinoth --password =dckr_pat_G6kRN127hG362EA-fDWLyDZTrRo
-docker tag app:v1 ilamathivinoth/dev:v1
-docker push ilamathivinoth/dev:v1
+docker login --username =ilamathivinoth --password-stdin <<<'dckr_pat_thfZomxqdadG564KxFMAVG1WABg' 
+docker tag app:v1 ilamathivinoth/dev
+docker push ilamathivinoth/dev
 if [[ $GIT_BRANCH == "main"]]; then
-docker tag app:v1 ilamathivinoth/prod:v1
-docker push ilamathivinoth/prod:v1
-elif[[ $GIT_BRANCH == "dev"]]; then
-docker tag app:v1 ilamathivinoth/dev:v1
-docker push ilamathivinoth/dev:v1
+docker tag app:v1 ilamathivinoth/prod
+docker push ilamathivinoth/prod
+elif [[ $GIT_BRANCH == "dev"]]; then
+docker tag app:v1 ilamathivinoth/dev
+docker push ilamathivinoth/dev
 else
 echo "pipeline successful"
 fi
