756a1be9b3be3365622489278e359ee37b36161d
ilamathi <ilamathi.ka@gmail.com>
2024-03-15 19:33:10 +0530

    commit msg

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,10 +3,10 @@
 docker login --username = ilamathivinoth --password = dckr_pat_thfZomxqdadG564KxFMAVG1WABg
 docker tag app:v1 ilamathivinoth/dev
 docker push ilamathivinoth/dev
-if [[ $GIT_BRANCH == "main"]]; then
+if [[ $GIT_BRANCH == "main" ]]; then
 docker tag app:v1 ilamathivinoth/prod
 docker push ilamathivinoth/prod
-elif [[ $GIT_BRANCH == "dev"]]; then
+elif [[ $GIT_BRANCH == "dev" ]]; then
 docker tag app:v1 ilamathivinoth/dev
 docker push ilamathivinoth/dev
 else
