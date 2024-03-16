b189782a7e02d312dd29c06cfd6a57fe45a2338e
ilamathi <ilamathi.ka@gmail.com>
2024-03-16 08:45:10 +0530

    commit

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -u “ilamathi.ka@gmail.com” -p “dckr_pat_thfZomxqdadG564KxFMAVG1WABg” docker.io
+docker login -u="${ilamathi.ka@gmail.com}" -p="${aadira@2022}"
 docker tag app:v1 ilamathivinoth/dev
 docker push ilamathivinoth/dev
 if [[ $GIT_BRANCH == "main" ]]; then
