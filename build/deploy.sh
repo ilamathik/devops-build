92d9e8b8839dbdc0805f5f269bf5a5ed0514c6c0
ilamathi <ilamathi.ka@gmail.com>
2024-03-16 18:14:05 +0530

    commit msg

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 docker login -u ilamathivinoth -p dckr_pat_4ROxiLP-UeHOLlTgQjnC2yfNbNU
-
-if [[ $GIT_BRANCH == "main" ]]; then
+if [[ $(GIT_BRANCH) == "main" ]]; then
 docker tag app:v1 ilamathivinoth/prod
 docker push ilamathivinoth/prod
-elif [[ $GIT_BRANCH == "dev" ]]; then
+elif [[ $(GIT_BRANCH) == "dev" ]]; then
 docker tag app:v1 ilamathivinoth/dev
 docker push ilamathivinoth/dev
 else
