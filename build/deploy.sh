8ed4d332a8cca824811f967dee19d2caeecbc2a5
ilamathik <154395583+ilamathik@users.noreply.github.com>
2024-03-18 10:27:21 +0530

    Update actual deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,7 +2,6 @@
 
 
 if [ "$GIT_BRANCH" = "origin/dev" ]; then
-echo "hello world"
 docker build -t ilamathivinoth/dev /var/lib/jenkins/workspace/New-project/build
 echo dckr_pat_4ROxiLP-UeHOLlTgQjnC2yfNbNU | docker login -u ilamathivinoth --password-stdin
 docker push ilamathivinoth/dev
